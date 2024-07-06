8ade211934515fb28025920398e8b80d63a37bd8
ignite312 <contact.emonkhan@gmail.com>
2024-07-07 04:35:40 +0600

    Coins, Stones

--- Coins.cpp
+++ Coins.cpp
@@ -5,4 +5,37 @@ Idea:
 Complexity:
 Resource:
 */
+#include<bits/stdc++.h>
+using namespace std;
 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int tt;
+    tt = 1;
+    // cin >> tt;
+    while(tt--) {
+        int n;
+        cin >> n;
+        vector<double> p(n);
+        for(int i = 0; i < n; i++) {
+          cin >> p[i];
+        }
+        vector<double> dp(n+1, 0);
+        dp[0] = 1.0;       // Probability of 0 Head
+        for(int i = 0; i < n; i++) {
+          for(int h = i+1; h >= 0; h--) {
+            if(h > 0)dp[h] = dp[h-1]*p[i] + dp[h]*(1-p[i]);
+            else dp[h] = dp[h]*(1-p[i]);
+          }
+        }
+        double ans = 0.00;
+        for(int i = 0; i <= n; i++) {
+          if(i > (n-i)) {
+            ans += dp[i];
+          }
+        }
+        cout << fixed << setprecision(12) << ans << "\n";
+    }
+    return 0;
+}
--- Stones.cpp
+++ Stones.cpp
@@ -5,4 +5,31 @@ Idea:
 Complexity:
 Resource:
 */
+#include<bits/stdc++.h>
+using namespace std;
 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int tt;
+    tt = 1;
+    // cin >> tt;
+    while(tt--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> pile(n);
+        vector<bool> dp(k+1, false);
+        for(int i = 0; i < n; i++) {
+            cin >> pile[i];
+        }
+        for(int i = 1; i <= k; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i-pile[j] >= 0 && !dp[i-pile[j]]) {
+                    dp[i] = true;
+                }
+            }
+        }
+        cout << (dp[k] ? "First\n" : "Second\n");
+    }
+    return 0;
+}
